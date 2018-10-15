63105710d3c6d72483d8f8a50ff1c4a9af34c416
grecoe <grecoe@microsoft.com>
2018-10-15 08:18:07 -0400

    environment setup for CICD

---
+++ envsetup/install_requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python --version
+pip install azureml-sdk[notebooks]==0.1.58
+pip install aiohttp==3.3.2
+pip install toolz==0.9.0
+pip install tqdm==4.23.4
+pip install azure-cli==2.0.41
+pip install lightgbm==2.1.2
+pip install papermill==0.14.1
+pip install git+https://github.com/theskumar/python-dotenv
+pip install -r requirements.txt
