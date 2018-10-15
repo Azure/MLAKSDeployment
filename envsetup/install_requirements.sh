253f9fe5bf5fd788648b1c865007552540895e8e
grecoe <grecoe@microsoft.com>
2018-10-15 10:22:40 -0400

    environment setup for CICD v3

--- envsetup/install_requirements.sh
+++ envsetup/install_requirements.sh
@@ -8,5 +8,5 @@ pip install azure-cli==2.0.41
 pip install lightgbm==2.1.2
 pip install papermill==0.14.1
 pip install git+https://github.com/theskumar/python-dotenv
-conda install -y -f nb_conda==2.2.0
+conda install -y -f -n testenv nb_conda==2.2.0
 pip install -r requirements.txt
